6d8f39a56e5d27aa4fab7a548f6a25c08cb87036
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2019-07-24 20:57:22 +0600

    functions practices
    Functions practices

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+register_nav_menu('main-menu','Main Menu');
+register_nav_menu('footer-menu','Footer Menu');
+
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body>
+		<?php bloginfo('title'); ?>
+		<br>
+		<?php bloginfo('description'); ?>
+		<br>
+		<?php bloginfo('url'); ?>
+		<br>
+		<?php bloginfo('name'); ?>
+		<br>
+		<?php bloginfo('home'); ?>
+		<br>
+		<?php bloginfo('admin_email'); ?>
+		<br>
+		<?php bloginfo('atom_url'); ?>
+		<br>
+		<?php bloginfo('charset'); ?>
+		<br>
+		<?php bloginfo('comments_atom_url'); ?>
+		<br>
+		<?php bloginfo('html_type'); ?>
+		<br>
+		<?php bloginfo('language'); ?>
+		<br>
+		<?php bloginfo('stylesheet_directory'); ?>
+		<br>
+		<?php bloginfo('stylesheet_url'); ?>
+		<br>
+		<?php bloginfo('template_directory'); ?>
+		<br>
+		<?php bloginfo('template_url'); ?>
+		<br>
+		<?php bloginfo('text_direction'); ?>
+		<br>
+		<?php bloginfo('version'); ?>
+		<br>
+		<?php bloginfo('wpurl'); ?>
+		<br>
+		
+
+		<?php wp_nav_menu(array(
+			'theme_location' => 'footer-menu'
+		)); ?>
+
+	<?php  wp_footer(); ?>
+</body>
+</html>
